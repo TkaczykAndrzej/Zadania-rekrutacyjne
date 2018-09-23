1406d0bd7201514cdf6769915d97f00dc2f94a2f
Andrzej Tkaczyk <andrzej.tkaczyk93@gmail.com>
2018-09-23 14:42:02 +0200

    Zadanie 1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            bool pc = false;
+            foreach (char znak in text)
+                if (Char.IsDigit(znak))
+                {
+                    pc = true;
+                    break;
+                }
+
+            if (pc)
+                Console.WriteLine("I'm not calculator");
+            else if (text.Equals("Date"))
+                Console.WriteLine(System.DateTime.Now.ToString());
+            else
+                Console.WriteLine("Hi!");
+            Console.ReadKey();
+        }
+    }
+}
